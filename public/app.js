e302295c8885d124419e5f311f10c266e0eef42b
Lewis <lewisburns630@googlemail.com>
2017-06-20 01:05:48 +0100

    Refactored version

--- public/app.js
+++ public/app.js
@@ -1,28 +1,70 @@
-var app = function() {
-//Create an <article> with class 'quote'
-  var ulCat =document.createElement('ul');
-  ulCat.classList.add('cat');
-// Create <liName>
-  var liName = document.createElement('li')
-  liName.innerText = "Name: Poppy"
+var cats = [
+  {
+    text: 'Name: Poppy\n',
+    favouriteFood: "Big Mac, Large fries and coke\n",
+    image: "[insert image here]"
+  }
+]
 
-// Create <lifood>
-  var liFood = document.createElement('li')
-  liFood.innerText = "Favourite Food: Big Mac, Large fries and a coke";
-  // goes down level by level like a path through tags
-  var catImage = document.createElement('li')
-  catImage.innerText = "Insert image here";
+var createcatul = function () {
+  // Create an <ul> with a class 'cat'
+  var catul = document.createElement('ul');
+  catul.classList.add('cat');
+  return catul;
+}
 
+var createliCat = function (text) {
+  // create <liCat> with text inside it
+  var liCat = document.createElement('liCat');
+  liCat.innerText = text;
+  return liCat;
+}
+
+var createliFood = function (catName) {
+  // Create a <liFood> with text inside it
+  var liFood = document.createElement('liFood');
+  liFood.innerText = catName;
+  return liFood;
+}
+
+var createliImage = function (catName) {
+  // Create a <liFood> with text inside it
+  var LiImage = document.createElement('liImage');
+  liImage.innerText = catName;
+  return liImage;
+}
+
+var appendElements = function (list, ul, liCat, liFood,liImage) {
  
+  // Put <liFood> in the <liCat>
+  liCat.appendChild(liFood);
+  liCat.appendChild(liImage);
+  // Put the <liCat> in the <ul>
+  ul.appendChild(liCat);
+  // Put full <ul> in the list of cats
+  list.appendChild(ul);
+}
 
- // then builds back up appending the changes
-  liName.appendChild(liFood);
-  liName.appendChild(catImage);
-  ulCat.appendChild(liName)
+var addcat = function(catName, catText,catImage) {
+  var catul = createcatul();
+  var liCat = createliCat(catText);
+  var liFood = createliFood(catName);
+  var liImage = createliFood(catImage);
 
-  var quoteList = document.querySelector('#cats')
-  quoteList.appendChild(ulCat)
+
+  var catsList = document.querySelector('#cats');
+  appendElements(catsList, catul, liCat, liFood,liImage);
 }
 
 
+var app = function () {
+
+  for (var cat of cats) {
+    addcat(cat.favouriteFood, cat.text,cat.image);
+  }
+
+}
+
 window.onload = app;
+
+
